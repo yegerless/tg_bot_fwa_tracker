7069a32f7ccc18d1a4157ed166f622fae7fa6c25
yegerless <Sergey.sys@mail.ru>
2025-01-16 22:53:45 +0300

    Add storage classes in storage.py

---
+++ bot/storage.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, ConfigDict
+
+
+
+class UserData(BaseModel):
+    weight: int
+    height: int
+    age: int
+    activity: int
+    city: str
+    water_goal: int
+    calorie_goal: int
+    logged_water: int
+    logged_calories: int
+    burned_calories: int
+
+
+
+class UserId(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    user_id: int
+
+
+
+class Storage(BaseModel):
+    users: dict[UserId, UserData] | None = None
+
+
+storage = Storage()
