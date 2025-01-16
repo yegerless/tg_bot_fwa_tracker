97324903d41cace676ae40541cacca6ce1030bfd
yegerless <Sergey.sys@mail.ru>
2025-01-16 19:03:21 +0300

    Add delete_webhook method on bot object in bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -13,10 +13,19 @@ async def main():
     # тут сделать логгирование при запуске бота
     
     bot = Bot(token=BOT_TOKEN)
+    
+    # Диспетчер, на который вешаются роутеры и мидлварь для логгирования
     dp = Dispatcher(storage=MemoryStorage())
     
     # dp.include_routers()
     
+    # тут повесить мидлварь для логгирования
+    
+    
+    # Пропуск всех накопленных входящих сообщений при запуске бота
+    await bot.delete_webhook(drop_pending_updates=True)
+    
+    # Запуск бота
     await dp.start_polling(bot)
 
 
