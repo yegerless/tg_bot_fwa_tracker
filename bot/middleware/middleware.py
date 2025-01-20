5a22f582cf72d76e1a11eb65164b1d0a513a98f9
yegerless <Sergey.sys@mail.ru>
2025-01-20 22:18:48 +0300

    Write docs and comments in middleware/middleware.py

--- bot/middleware/middleware.py
+++ bot/middleware/middleware.py
@@ -3,25 +3,43 @@ from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
 from loguru import logger
 
-logger.add("logs.log", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", level="INFO", rotation="100 MB")
+# Добавление обработчика с сохранением логов бота в файл с ротацией по размеру файла
+logger.add("logs.log", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", 
+           level="INFO", rotation="100 MB")
 
 
 
 class LoguruMiddleware(BaseMiddleware):
-    ''' Докстринга '''
+    '''
+        Мидлварь, которая логгирует все полученные от пользователя сообщения,
+        которые были обработаны хэндлерами.
+    '''
 
     def __init__(self, router_name: str | None = None):
-        ''' Докстринга '''
-        
+        '''
+            Конструктор экземпляров мидлвари. 
+            Принимает аргумент router_name - имя роутера, на который 
+                добавляется экземпляр мидлвари.
+
+            Прим.: это имитация легаси кода в проекте, на самом деле
+                функционал мидлвари, для которого нужно было имя роутера,
+                я выпилил еще в середине работы, а про этот конструктор 
+                вспомнил только в самом конце...
+        '''
+
         self.router_name = router_name
 
 
     async def __call__(self, handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
                        event: TelegramObject, data: Dict[str, Any]) -> Any:
-        ''' Докстринга '''
-        
-        if data.get('command'):
+        ''' 
+            Метод __call__ реализует возможность экземпляров  мидлвари быть вызванными
+                как функция.
+            Здесь реализован функционал логгирования пойманных сообщений.
+        '''
+
         # Логгирование момента получения сообщения и отправки ответа
+        if data.get('command'):
             logger.info(f'Message received {data.get('command').command}')
         else:
             logger.info(f'Message received {data.get('event_update').message.text}')
