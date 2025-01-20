33342542f07e2d6b939e915e3c011d75078e382e
yegerless <Sergey.sys@mail.ru>
2025-01-20 22:37:05 +0300

    Write docs and comments in bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -8,33 +8,33 @@ from handlers.base_handlers import base_router
 from handlers.profile import profile_router
 from handlers.tracker import tracker_router
 
+
+# Добавление обработчика с сохранением логов бота в файл с ротацией по размеру файла
 logger.add("logs.log", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", level="INFO", rotation="100 MB")
 
 
-
 async def main():
-    ''' Написать докстрингу '''
-    
+    ''' 
+        Основная функция, которая запускает бота.
+    '''
+
     # Логгирование при запуске бота
     logger.info('The bot has STARTED')
-    
+
+    # Объект бота с его настройками
     bot = Bot(token=BOT_TOKEN)
-    
-    # Диспетчер, на который вешаются роутеры и мидлварь для логгирования
+
+    # Диспетчер, на который вешаются роутеры
     dp = Dispatcher(storage=MemoryStorage())
-    
     dp.include_routers(base_router, profile_router, tracker_router)
-    
-    # тут повесить мидлварь для логгирования
-    
+
     # Пропуск всех накопленных входящих сообщений при запуске бота
     await bot.delete_webhook(drop_pending_updates=True)
-    
     # Запуск бота
     await dp.start_polling(bot)
-    
+
+    # Логгирование при остановке бота
     logger.info('The bot has STOPPED')
-    
 
 
 if __name__ == '__main__':
