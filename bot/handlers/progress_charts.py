75005ccb59fcf2a93fdc355eb52c0ab32d0235d6
yegerless <Sergey.sys@mail.ru>
2025-01-21 17:34:55 +0300

    Add calories_chart handler in handlers/progress_charts.py

--- bot/handlers/progress_charts.py
+++ bot/handlers/progress_charts.py
@@ -5,7 +5,7 @@ from aiogram.filters import Command
 
 from storage import storage
 from middleware.middleware import LoguruMiddleware
-from utils.charts import get_water_chart
+from utils.charts import get_water_chart, get_kalories_chart
 
 
 # Содание роутера и прикрепление к нему логгера
@@ -52,35 +52,39 @@ async def water_chart(message: Message):
 async def calories_chart(message: Message):
     ''' Докстринга '''
 
-    date = datetime.today().strftime('%d-%m-%Y')
+    dates = []
+    for i in range(0, 7):
+        dates.append((datetime.today() - timedelta(days=i)).strftime('%d-%m-%Y'))
 
     user_id = message.from_user.id
     user_data = storage.get(user_id)
     if user_data:
-        # Данные за последние 7 дней
-        logged_kalories = user_data.get('logged_calories')
+        # Данные по воде за последние 7 дней, сразу рассчитывает сумму по дням
+        logged_kalories = {}
+        burned_kalories = {}
+        for date in dates:
+            if date in user_data.get('logged_calories'):
+                logged_kalories[date] = sum(user_data.get('logged_calories').get(date).values())
+            else:
+                logged_kalories[date] = 0
+            if date in user_data.get('burned_calories'):
+                burned_kalories[date] = sum(user_data.get('burned_calories').get(date).values())
+            else:
+                burned_kalories[date] = 0
+    else:
+        # Если профиля пользователя нет в хранилище, то предлагаем создать профиль
+        await message.answer(text='Пожалуйста создайте Ваш профиль при помощи команды /set_profile.')
+        return None
 
-        # Только если данные были введены сегодня хотя бы раз
-        if logged_kalories:
-            logged_kalories = sum(logged_kalories.values())
-
-    await message.answer(text='OK')
-
-
-@charts_router.message(Command('burned_calories_chart'))
-async def burned_calories_chart(message: Message):
-    ''' Докстринга '''
-
-    date = datetime.today().strftime('%d-%m-%Y')
-
-    user_id = message.from_user.id
-    user_data = storage.get(user_id)
-    if user_data:
-        # Данные за последние 7 дней
-        burned_kalories = user_data.get('burned_calories')
-
-        # Только если данные были введены сегодня хотя бы раз
-        if burned_kalories:
-            burned_kalories = sum(burned_kalories.values())
-
-    await message.answer(text='OK')
+    # Только если данные есть данные по воде хотя бы за один день
+    if logged_kalories or burned_kalories:
+        chart = get_kalories_chart(logged_kalories, burned_kalories, user_data.get('kalories_goal'))
+        chart = await message.answer_photo(
+            BufferedInputFile(chart, filename='kalories_chart.png'),
+            caption='График полученных калорий',
+            show_caption_above_media=True
+        )
+    else:
+        await message.answer(text=('Последние 7 дней вы не сохраняли данные о '
+                             'съеденных продуктах.')
+                             )
--- bot/utils/charts.py
+++ bot/utils/charts.py
@@ -9,9 +9,32 @@ def get_water_chart(logged_water: dict[str, int], water_goal: int) -> None:
                          marker_color='CornflowerBlue', text=list(logged_water.values())))
     fig.update_layout(title_text='Кол-во выпитой воды за последние 7 дней.',
                       xaxis_title='Дата', yaxis_title='Выпито воды в мл', 
-                      yaxis_range=[0, water_goal + 1000], plot_bgcolor='AliceBlue')
+                      yaxis_range=[0, water_goal + 1000], plot_bgcolor='AliceBlue',
+                      autosize=False, width=800, height=500, 
+                      margin={'l': 10, 'r': 10, 'b': 25, 't': 50})
     fig.add_hline(y=water_goal, line_color='red', annotation_text=water_goal)
     chart = fig.to_image(format='png')
 
     return chart
-    
+
+
+def get_kalories_chart(logged_kalories: dict[str, int], burned_kalories: dict[str, int], 
+                       kalories_goal: int) -> bytes:
+    ''' Докстринга '''
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=list(logged_kalories.keys()), y=list(logged_kalories.values()),
+                         marker_color='Crimson', text=list(logged_kalories.values()),
+                         name='Полученные калории'))
+    fig.add_trace(go.Bar(x=list(burned_kalories.keys()), y=list(burned_kalories.values()),
+                         marker_color='Orange', text=list(burned_kalories.values()),
+                         name='Сожженные калории'))
+    fig.update_layout(title_text='Баланс калорий за последние 7 дней.',
+                      xaxis_title='Дата', yaxis_title='Калории', 
+                      yaxis_range=[0, kalories_goal + 1000], plot_bgcolor='AliceBlue',
+                      autosize=False, width=800, height=500, 
+                      margin={'l': 10, 'r': 10, 'b': 25, 't': 50})
+    fig.add_hline(y=kalories_goal, line_color='Red', annotation_text=kalories_goal)
+    chart = fig.to_image(format='png')
+
+    return chart
