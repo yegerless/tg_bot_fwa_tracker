a9bcb8e12050e5c3ff6253991afb112391cae745
yegerless <Sergey.sys@mail.ru>
2025-01-21 17:51:00 +0300

    Add docs and comments in utils/charts.py

--- bot/utils/charts.py
+++ bot/utils/charts.py
@@ -1,18 +1,31 @@
 import plotly.graph_objects as go
 
 
-def get_water_chart(logged_water: dict[str, int], water_goal: int) -> None:
-    ''' Докстринга '''
+def get_water_chart(logged_water: dict[str, int], water_goal: int) -> bytes:
+    '''
+        Функция get_water_chart - принимает данные о выпитой воде по 
+            дням и цель по выпитой воде в день, возвращает изображение
+            графика выпитой воды по дням в формате png в виде байтового
+            объекта (который затем можно отправить пользователю).
+        Аргументы:
+            logged_water (dict(str: int)) - данные о выпитой воде по дням в
+                виде словаря в формате {день: кол-во воды в мл}.
+            water_goal (int) - цель пользователя по выпитой воде в день в мл.
+    '''
 
+    # Создание бар-чарта
     fig = go.Figure()
     fig.add_trace(go.Bar(x=list(logged_water.keys()), y=list(logged_water.values()),
                          marker_color='CornflowerBlue', text=list(logged_water.values())))
+    # Настройка шаблона графика (подписи, размер и тд)
     fig.update_layout(title_text='Кол-во выпитой воды за последние 7 дней.',
                       xaxis_title='Дата', yaxis_title='Выпито воды в мл', 
                       yaxis_range=[0, water_goal + 1000], plot_bgcolor='AliceBlue',
                       autosize=False, width=800, height=500, 
                       margin={'l': 10, 'r': 10, 'b': 25, 't': 50})
+    # Добавление целевой линии
     fig.add_hline(y=water_goal, line_color='red', annotation_text=water_goal)
+    # Конфертация графика изображение в формате png в виде байтового объекта
     chart = fig.to_image(format='png')
 
     return chart
@@ -20,21 +33,38 @@ def get_water_chart(logged_water: dict[str, int], water_goal: int) -> None:
 
 def get_kalories_chart(logged_kalories: dict[str, int], burned_kalories: dict[str, int], 
                        kalories_goal: int) -> bytes:
-    ''' Докстринга '''
+    '''
+        Функция get_kalories_chart - принимает данные о полученных и 
+            сожженных калориях по дням и цель по калориям в день, 
+            возвращает изображение графика полученных и потраченных
+            калорий по дням в формате png в виде байтового
+            объекта (который затем можно отправить пользователю).
+        Аргументы:
+            logged_kalories (dict(str: int)) - данные о полученных калориях 
+                по дням в виде словаря в формате {день: кол-во калорий}.
+            burned_kalories (dict(str: int)) - данные о сожженных калориях
+                по дням в виде словаря в формате {день: кол-во калорий}.
+            water_goal (int) - цель пользователя по калориям в день.
+    '''
 
+    # Создание бар-чарта
     fig = go.Figure()
     fig.add_trace(go.Bar(x=list(logged_kalories.keys()), y=list(logged_kalories.values()),
                          marker_color='Crimson', text=list(logged_kalories.values()),
                          name='Полученные калории'))
+    # Создание второго бар-чарта, который застакается с первым
     fig.add_trace(go.Bar(x=list(burned_kalories.keys()), y=list(burned_kalories.values()),
                          marker_color='Orange', text=list(burned_kalories.values()),
                          name='Сожженные калории'))
+    # Настройка шаблона графика (подписи, размер и тд)
     fig.update_layout(title_text='Баланс калорий за последние 7 дней.',
                       xaxis_title='Дата', yaxis_title='Калории', 
                       yaxis_range=[0, kalories_goal + 1000], plot_bgcolor='AliceBlue',
                       autosize=False, width=800, height=500, 
                       margin={'l': 10, 'r': 10, 'b': 25, 't': 50})
+    # Добавление целевой линии
     fig.add_hline(y=kalories_goal, line_color='Red', annotation_text=kalories_goal)
+    # Конфертация графика изображение в формате png в виде байтового объекта
     chart = fig.to_image(format='png')
 
     return chart
